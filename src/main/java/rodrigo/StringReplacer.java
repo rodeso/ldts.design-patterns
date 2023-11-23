5d364e4f5d3641e05d43f74b12854f0153f06308
Rodrigo de Sousa <up202205751@up.pt>
2023-11-23 00:15:21 +0000

    Command Design Pattern: Implemented

--- src/main/java/rodrigo/StringReplacer.java
+++ src/main/java/rodrigo/StringReplacer.java
@@ -1,4 +1,16 @@
 package rodrigo;
 
 public class StringReplacer implements StringTransformer{
+    private char targetChar;
+    private char replacementChar;
+
+    public StringReplacer(char targetChar, char replacementChar) {
+        this.targetChar = targetChar;
+        this.replacementChar = replacementChar;
+    }
+    @Override
+    public void execute(StringDrink drink) {
+        // Using String.replace(char, char) to replace occurrences of targetChar with replacementChar
+        drink.setText(drink.getText().replace(targetChar, replacementChar));
+    }
 }
